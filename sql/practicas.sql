f37f932f2e4ff3029efd62740eda7b4d25bdc64c
brn2bwild <brn_2_b_wild@hotmail.com>
2019-12-12 14:31:56 -0600

    base de datos modificada

--- sql/practicas.sql
+++ sql/practicas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2019 a las 20:05:12
+-- Tiempo de generación: 12-12-2019 a las 21:31:08
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.10
 
